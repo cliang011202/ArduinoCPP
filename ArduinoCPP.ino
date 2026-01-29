7cee7cc6e8cb671b5f9b609cd25af3f6a485c1b0
SYSU_LC <liangch93@mail2.sysu.edu.cn>
2026-01-29 13:41:10 +0800

    Day6 从单任务loop迈向多任务思维

--- ArduinoCPP.ino
+++ ArduinoCPP.ino
@@ -1,26 +1,62 @@
 #include "Led.hpp"
-#include "LedStateMachine.hpp"
+// #include "LedStateMachine.hpp"
 
 constexpr uint8_t LED_PIN = 13;
-constexpr unsigned long INTERVAL_MS = 500;
+
+enum class LedState {
+  OFF,
+  ON
+};
 
 Led led(LED_PIN);
-LedStateMachine led_fsm(led);
+LedState led_state = LedState::OFF;
+
+void updateLedStateMachine(bool timeout_event);
+void taskLed();
+void taskDebug();
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  taskLed();
+  taskDebug();
+}
+
+void taskLed() {
   static unsigned long last_time = 0;
-
+  constexpr unsigned long INTERVAL_MS = 1000;
+  
   unsigned long now = millis();
-  bool timeout_event = false;
-
   if(now - last_time >= INTERVAL_MS){
     last_time = now;
-    timeout_event = true;
+    updateLedStateMachine(true);
   }
+}
 
-  led_fsm.(timeout_event);
+void taskDebug() {
+  static unsigned long last_time = 0;
+  constexpr unsigned long INTERVAL_MS = 2000;
+  
+  unsigned long now = millis();
+  if(now - last_time >= INTERVAL_MS){
+    last_time = now;
+    Serial.println("System alive");
+  }
+}
+
+void updateLedStateMachine(bool timeout_event){ 
+  if(!timeout_event) return; 
+
+  switch (led_state) { 
+    case LedState::OFF:
+      led.on();
+      led_state = LedState::ON; 
+      break; 
+    case LedState::ON: 
+      led.off(); 
+      led_state = LedState::OFF; 
+      break; 
+  }
 }
